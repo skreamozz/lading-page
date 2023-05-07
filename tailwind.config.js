37fa3a4d56b3f0716c2d6aef50396e8d78bf424e
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-05-07 16:18:17 -0500

    hice un carrusel rudimentario, para un futuro se espera cambiarlo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,17 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
+      screens: {
+        "500px": "500px",
+      },
       backgroundImage: {
         "logo-facebook": "url(../src/imgs/facebook.png)",
         "logo-instagram": "url(../src/imgs/instagram.png)",
         "logo-twitter": "url(../src/imgs/twitter.png)",
         "logo-user": "url(../src/imgs/user.png)",
+        "img-1": "url(../src/imgs/img1.jpg)",
+        "img-2": "url(../src/imgs/img2.jpg)",
+        "img-3": "url(../src/imgs/img3.jpg)",
       },
     },
   },
