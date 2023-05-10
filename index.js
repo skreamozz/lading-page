50269242111ad396e520157d2e082e510c4e7bbe
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-05-09 21:10:41 -0500

    hice el menu

---
+++ index.js
@@ -0,0 +1,26 @@
+const buttonMenu = document.getElementById("button-menu");
+const itemsMenu = document.getElementById("items-menu");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+
+let activeMenu = false;
+
+buttonMenu.addEventListener("click", () => {
+  activeMenu = !activeMenu;
+
+  if (activeMenu) {
+    itemsMenu.className =
+      "absolute top-12 right-0 w-[70%] h-screen bg-black text-white transition-all  duration-300 z-50";
+
+    header.className = "mb-8 relative overflow-x-visible";
+
+    setTimeout(() => {}, 350);
+  } else {
+    header.className = "mb-8 relative overflow-x-hidden";
+
+    itemsMenu.className =
+      "absolute top-12 -right-[1000px] w-[70%] h-screen bg-black text-white transition-all duration-300 z-50";
+
+    setTimeout(() => {}, 350);
+  }
+});
