3f6a25a61282ea309fa1fa21d65b0e884e58b302
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-05-10 21:27:31 -0500

    agregue la sección de comunidad con un video

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       screens: {
         "500px": "500px",
+        "400px": "400px",
+        "500px": "500px",
         "700px": "700px",
       },
       backgroundImage: {
@@ -15,10 +17,6 @@ module.exports = {
         "img-1": "url(../src/imgs/img1.jpg)",
         "img-2": "url(../src/imgs/img2.jpg)",
         "img-3": "url(../src/imgs/img3.jpg)",
-      },
-
-      boxShadow: {
-        "shadow-employee": "",
       },
     },
   },
